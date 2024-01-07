c8ed583968ffae244e9b984814163ac49b02bdd3
M4tth142 <matthias.polen@student.hogent.be>
2024-01-07 22:23:24 +0100

    summary

---
+++ ParkDataLayer/Model/Huis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkDataLayer.Model
+{
+    public class Huis
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Straat { get; set; }
+
+        [Required]
+        public int Nummer { get; set; }
+
+        [Required]
+        public bool Actief { get; set; }
+    }
+}
---
+++ ParkDataLayer/Model/Huurder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkDataLayer.Model
+{
+    public class Huurder
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Naam { get; set; }
+
+        [MaxLength(100)]
+        public string Telefoon { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [MaxLength(100)]
+        public string Adres { get; set; }
+    }
+}
